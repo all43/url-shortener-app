3e9721ff863afbaeda5384681bd8927d9e68037e
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2021-03-22 12:06:54 +0100

    clear on submit & disabled state

--- src/components/InputBlock.vue
+++ src/components/InputBlock.vue
@@ -1,13 +1,17 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <input type="text" v-model="url">
-    <button type="submit">Submit</button>
+    <button type="button" :disabled="isDisabled" @click="clear">Clear</button>
+    <button type="submit" :disabled="isDisabled">Submit</button>
   </form>
 </template>
 
 <script>
 export default {
   name: 'InputBlock',
+  props: {
+    disabled: { type: Boolean, requred: false },
+  },
   data: () => ({
     url: 'https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch',
   }),
@@ -16,9 +20,15 @@ export default {
       this.url = '';
     },
     handleSubmit() {
+      if (this.isDisabled) return;
       const { url } = this;
       this.$emit('create', { url });
       this.clear();
+    },
+  },
+  computed: {
+    isDisabled() {
+      return this.disabled || !this.url.trim().length;
     },
   },
 };
